d6bc5d4cc82afea8f5233ef1022dc3a54ceb575e
silentShulk <marcostrada258@gmail.com>
2026-06-01 14:57:01 +0200

    Updated directories

--- Arch(Cachy)/setup-directories.sh
+++ Arch(Cachy)/setup-directories.sh
@@ -1,23 +1,18 @@
 #! /bin/bash
 
-# Downloading repos
+# Folder for external repos
 mkdir ~/Repos
-mkdir ~/MyRepos
 
-cd ~/MyRepos
-
+# Copying my own repos
+cd ~/Projects/
 gh repo clone silentShulk/MyZed
 gh repo clone silentShulk/Accord-Timeline-Alterer
 gh repo clone silentShulk/Linux-Setup
-
-# Downloading dotfiles repo
 gh repo clone silentShulk/Hyprland-Dots
 
 # Setting up manim venv
 uv python install
-
 cd ~/
-
 uv init --python 3.13 ManimProjects
 cd ManimProjects
 uv add manim opencv-python pillow
